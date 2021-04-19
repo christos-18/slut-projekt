5a660ae105d73f2287fd784cb2f06f125cc4833d
christos <potamitischristos@gmail.com>
2021-04-19 08:48:49 +0200

    1

--- slutprojekt/Program.cs
+++ slutprojekt/Program.cs
@@ -1,12 +1,103 @@
 ﻿using System;
 
-namespace slutprojekt
+namespace Slutprojekt
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            int continueplaying = 1;
+
+            RunGame();
+
+            while (continueplaying == 1)
+            {
+
+                Console.WriteLine("Vill du spela igen, ja eller nej?", Console.ForegroundColor = ConsoleColor.White);
+
+
+                string answer = Console.ReadLine();
+
+                if (answer.ToLower() == "ja")
+                {
+                    Console.Clear();
+
+                    RunGame();
+                }
+                else if (answer.ToLower() == "nej")
+                {
+                    continueplaying = 0;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Ogiltigt svar! \n");
+
+                }
+
+
+            }
         }
+
+
+
+
+
+        static void RunGame()
+        {
+
+
+
+
+            Console.WriteLine("Tryck Enter en gång för att starta spelet");
+
+
+           
+
+                Console.WriteLine("\n ");
+
+                Console.ReadLine();
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
     }
-}
+
+
+
+
+
+
+
+
